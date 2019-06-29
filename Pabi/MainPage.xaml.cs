9f538d20ed27b10c15bd367ac44ec570bb68f37d
José Rafael Ramón Navarro <jrafa.ramon@outlook.com>
2019-06-29 22:21:45 +0200

    Initial Commit

---
+++ Pabi/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Pabi
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await CameraPreview.StartAsync();
+            CameraPreview.CameraHelper.FrameArrived += CameraHelper_FrameArrived;
+        }
+
+        private void CameraHelper_FrameArrived(object sender, Microsoft.Toolkit.Uwp.Helpers.FrameEventArgs e)
+        {
+
+        }
+    }
+}
